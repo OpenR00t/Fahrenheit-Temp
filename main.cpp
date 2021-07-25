8bdc0448b541b813126a971fb13b1b123d493e84
OpenR00T <rpclaird@gmail.com>
2021-07-25 15:18:03 -0400

    establish citytemp class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+class CityTemps {
+    public:
+        void SetName(string cityName);
+        void SetFahrenheit(int FTemp);
+        void SetCelsius (int CTemp);
+        string GetName();
+        int GetFahrenheit();
+        int GetCelsius();
+
+    private:
+        string name;
+        int fahrenheit;
+        int celsius;
+};
+
+void CityTemps::SetName(string cityName)  {
+    name = cityName;
+}
+
+void CityTemps::SetFahrenheit(int Ftemp) {
+    fahrenheit = Ftemp;
+}
+
+void CityTemps::SetCelsius(int Ctemp) {
+    celsius = Ctemp;
+}
+
+string CityTemps::GetName() {
+    return name;
+}
+
+int CityTemps::GetFahrenheit() {
+    return fahrenheit;
+}
+
+int CityTemps::GetCelsius() {
+    return celsius;
+}
 
 int main() {
    
